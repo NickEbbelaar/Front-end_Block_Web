cb1a873184a0a24b6ab1ad31abe8420ad533a52f
NickEbbelaar <74213889+NickEbbelaar@users.noreply.github.com>
2021-01-21 09:59:18 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,50 @@
 // JavaScript Document
+
+/* Het hamburger menu */
+var menuBtn = document.querySelector('.menu-btn');
+menuBtn.addEventListener('click',menuOpen);
+
+function menuOpen() {
+   var hetmenu = document.querySelector('body > header > nav');
+   hetmenu.classList.toggle ('open');
+}
+
+/* De search button */
+var searchBtn = document.querySelector('.search-btn');
+searchBtn.addEventListener('click',searchopen);
+
+function searchopen() {
+   var demenu = document.querySelector('body > header > nav');
+   demenu.classList.toggle ('open2');
+}
+
+
+/* Het filter menu pop up
+var sortBtn = document.querySelector('.sort-btn');
+sortBtn.addEventListener('click',sortby);
+
+function sortby() {
+   var sortmenu = document.querySelector('#body > main > section');
+   sortmenu.classList.toggle ('open3');
+}
+
+*/
+
+
+
+
+
+
+
+/*
+let menuOpen = false;
+menuBtn.addEventListener('click',() => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
+*/
